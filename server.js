d89b767a099a589f7f9d087bb299f1473ffb8575
lukeblyons <lukeblyons@gmail.com>
2023-03-22 12:26:30 -0600

    Now serves front end files from back end end points

--- server.js
+++ server.js
@@ -2,9 +2,24 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-
 app.use(express.json());
 app.use(cors());
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+})
+
+app.get('/css', (req, res) => {
+    res.sendFile(__dirname + '/public/styles.css');
+})
+
+app.get('/js', (req, res) => {
+    res.sendFile(__dirname + '/public/main.js');
+})
+
+
+
+
 
 app.listen(5150, () => {
     console.log('Server running on port 5150');
